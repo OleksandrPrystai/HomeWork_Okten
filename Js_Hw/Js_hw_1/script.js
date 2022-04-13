500a1af2108d12dc0b0fe545be6b634555128cbb
OleksandrPrystai <prystaioleksandr@gmail.com>
2022-04-14 01:14:52 +0300

    Js homework 1

---
+++ Js_Hw/Js_hw_1/script.js
@@ -0,0 +1,110 @@
+// let st1 = "hello";
+// let st2 = "owu";
+// let st3 = "com";
+// let st4 = "ua";
+// let nb1 = 1;
+// let nb2 = 10;
+// let nb3 = -999;
+// let nb4 = 123;
+// let p = 3.14;
+// let nb5 = 2.7;
+// let nb6 = 16;
+// let bl1 = true;
+// let bl2 = false;
+// console.log(`${st1} ${st2} ${st3} ${st4}`);
+// console.log(nb1, nb2, nb3, nb4, p, nb5, nb6, bl1, bl2);
+// document.write(`${st1} ${st2} ${st3} ${st4} ${nb1} ${nb2} ${nb3} ${nb4} ${p} ${nb5} ${nb6} ${bl1} ${bl2} `);
+// alert(`${st1} ${st2} ${st3} ${st4} ${nb1} ${nb2} ${nb3} ${nb4} ${p} ${nb5} ${nb6} ${bl1} ${bl2} `)
+
+// let book1 = {
+//     name: "The Lord of the Rings",
+//     numberOfPages: 700,
+//     genre: "High fantasy " + "Adventure",
+// }
+// let book2 = {
+//     name: "The Hobbit",
+//     numberOfPages: 650,
+//     genre: "High fantasy",
+//     author: ["J.R.R. Tolkien " + "and " + "J.R.R. Tolkien"]  // Автор 1, тому дублянув два рази.
+// }
+// console.log(book2.author);
+
+// let firstName = "Oleksandr ";
+// let lastName = "Prysti ";
+// let middleName = "Leonidovich";
+//
+// let person = lastName + firstName +  middleName;
+// console.log(person)
+
+// let name = prompt("Enter you name");
+// let surname = prompt("Enter you surname");
+// let age = parseInt(prompt("Enter you age"));
+// console.log(name, surname, age);
+// console.log(name);
+// console.log(surname);
+// console.log(age);
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a, typeof b, typeof c);
+
+// let books = [
+//     {name: "The Hobbit", numberOfPages: 650, genre: "High fantasy", author: "J.R.R. Tolkien"},
+//     {name: "The Lord of the Rings", numberOfPages: 700, genre: "High fantasy", author: "J.R.R. Tolkien"},
+//     {name: "The Mysterious Island", numberOfPages: 600, genre: "Adventure novel", author: "Jules Verne"},
+//     {name: "Twenty Thousand Leagues Under the Sea ", numberOfPages: 1000, genre: "Science Fiction", author: "Jules Verne"},
+//     {name: "Robinson Crusoe", numberOfPages: 350, genre: "Novel", author: "Daniel Defoe"},
+// ]
+// console.log(books [1].name);
+// console.log(books [4].author);
+// console.log(books [3]);
+
+// let height = parseFloat(prompt("Enter height"));
+// let wight = parseFloat(prompt("Enter height"));
+// let s = height *wight;
+// alert(s + " Your area of the rectangle");
+
+// let height = parseFloat(prompt("Enter you height"));
+// let dc = parseFloat(prompt("Enter you diameter"));
+// let r = dc / 2;
+// let v = 3.14 * Math.pow(r, 2)*height;
+// alert(v + " Your cylinder volume")
+
+// let n = prompt("Введіть сторону n");
+// let m = prompt("Введіть сторону m");
+// let nm = Math.pow(n,2)+Math.pow(m,2);
+// let c = Math.sqrt(nm)
+// alert("Гіпотенуза прямокутного трикутника = " + c);
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 >= 6);
+// console.log(5 == 6);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(10 < 10);
+// console.log(10 != 10);
+// console.log(10 !== 10);
+// console.log(123 === "123");
+// console.log(123 == "123");
+
+// ERRORS
+
+// let a = 100;
+// let b = 500;
+// let c = " hello";
+// let d = " okten";
+// let x = ' constant value';
+// {
+//     console.log(a);
+//     x = 'new value';
+//     let b = 'new value';
+// }
+//
+// let result = 100 + b + c + d +x;
+//
+// console.log(result);
+
+
+
