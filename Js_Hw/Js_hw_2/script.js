1a3204294aa70048a62f33759359edd7ad4e2bd6
OleksandrPrystai <prystaioleksandr@gmail.com>
2022-04-19 01:56:26 +0300

    Js homework 2

---
+++ Js_Hw/Js_hw_2/script.js
@@ -0,0 +1,72 @@
+// let x = +prompt("Enter your number")
+// let a = x;
+// if (a === 0) {
+//     console.log("Невірно");
+// } else {
+//     console.log("Вірно");
+// }
+
+// let time = +prompt("Виберіть число від 0 до 60");
+//
+// if (time >= 0 && time <= 15) {
+//     console.log("Ви потрапили в першу чверть години");
+// } else if (time > 15 && time <= 30) {
+//     console.log("Ви потрапили в другу чверть години");
+// } else if (time > 30 && time <= 45) {
+//     console.log("Ви потрапили в третю чверть години");
+// } else if (time > 45 && time <= 60) {
+//     console.log("Ви потрапили в четверту чверть години");
+// } else {
+//     console.log("Ваше число не відповідає критеріям вводу");
+// }
+
+// let day = +prompt("Введіть день місяця");
+//
+// if (day > 0 && day <= 10) {
+//     console.log("Ви потрапили в першу третину місяця");
+// } else if (day > 10 && day <= 20) {
+//     console.log("Ви потрапили в другу третину місяця");
+// } else if (day > 20 && day <= 31) {
+//     console.log("Ви потрапили в третю третину місяця"); //гг третю третину; :)
+// } else {
+//     console.log("Ваше число не відповідає дійсності");
+// }
+
+// let day = +prompt("Введіть день тижня цифрою.");
+//
+// switch (day) {
+//     case 1 :
+//         document.write("Заняття js о 20:00")
+//         break;
+//     case 2 :
+//         document.write("Роз'яснення дз о 19:00")
+//         break;
+//     case 3 :
+//         document.write("Друге заняття js о 20:00")
+//         break;
+//     case 4 :
+//         document.write("Роз'яснення другого дз о 19:00")
+//         break;
+//     case 5 :
+//         document.write("Закріплення вивченого матеріалу за тиждень")
+//         break;
+//     case 6 :
+//         document.write("Англійська о 11:00")
+//         break;
+//     case 7 :
+//         document.write("Паска , шо буш робить??")
+//         break;
+//     default :
+//         document.write("Ви ввели неправильний день тижня")
+// }
+
+// let numb1 = +prompt("Введіть перше число");
+// let numb2 = +prompt("Введіть друге число");
+//
+// if (numb1 > numb2) {
+//     console.log(numb1,"більше ніж" , numb2);
+// } else if (numb2 > numb1) {
+//     console.log(numb2,"більше ніж", numb1);
+// } else if (numb1 = numb2) {
+//     console.log(numb1,"дорівнює",numb2)
+// }
