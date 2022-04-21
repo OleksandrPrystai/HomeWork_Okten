b54c90c5052001f2dc34ac0d2ff0abaf2d39c177
OleksandrPrystai <prystaioleksandr@gmail.com>
2022-04-22 00:20:23 +0300

    Js homework 3

---
+++ Js_Hw/Js_hw_3/script.js
@@ -0,0 +1,111 @@
+// for (let i = 0; i < 10 ; i++) {
+//     document.write(`<div>Hello mentor</div>`);
+// }
+
+// for (let i = 1; i < 11; i++) {
+//     document.write(`<div>Hello mentor ${i}</div>`);
+// }
+
+// let i = 0
+// while (i < 20){
+//     document.write(`<h4>hello okten</h4>`);
+//     i++
+// }
+
+// let i = 1;
+// while (i<21){
+//     document.write(`<h4>hallo mentor ${i}</h4>`)
+//     i++
+// }
+//
+
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// document.write(`<ul>`);
+// for (let i = 0; i < listOfItems.length; i++) {
+//     document.write(`<li>${listOfItems[i]}</li>`);
+// }
+// document.write(`</ul>`);
+
+//
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (let i = 0; i < products.length; i++) {
+//     let product = products[i];
+//     document.write(`<div>${product.title} ${product.price} <img class="image" src="${product.image}" alt=""></div>`)
+// }
+
+// let array = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// let i = 0
+// while (i<array.length) {
+//     document.write(array[i]);
+//     i++
+// }
+
+// for (let i = 0 ; i < array.length; i++) {
+//     document.write(array[i])
+// }
+
+// let i = 1;
+// while (i < array.length) {
+//     console.log(array[i]);
+//     i += 2
+// }
+
+// for (let i = 1; i < array.length; i += 2) {
+//     const numbers = array[i];
+//     console.log(numbers);
+// }
+
+// let i = 0
+// while (i < array.length) {
+//     console.log(array[i]);
+//     i += 2
+// }
+
+// for (let i = 0; i < array.length; i += 2) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement);
+// }
+
+// for (let i = array.length -1 ; i >= 0; i--) {
+//     console.log(array[i]);
+// }
+
+// let i = array.length - 1;
+// while (i >= 0){
+//     console.log(array[i])
+//     i--
+// }
+
+// for (let i = array.length -2; i >= 0  ; i-=2){
+//     console.log(array[i])
+// }
+
+// let i = array.length - 2;
+// while (i >= 0){
+//     console.log(array[i]);
+//     i-=2;
+// }
