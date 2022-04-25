20fee4374be50a82cca3cbce308b53f0a8bcc450
OleksandrPrystai <prystaioleksandr@gmail.com>
2022-04-26 01:31:11 +0300

    Js homework 4

---
+++ Js_Hw/Js_hw_4/script.js
@@ -0,0 +1,111 @@
+// function sumElements(a,b) {
+//
+//     let result = (a*b);
+//     return result;
+//
+// }
+// let numberS = sumElements(2, 10);
+// console.log(numberS)
+
+// function areaR(r) {
+//
+//     let result =  3.14 * r * r;
+//     return result;
+//
+// }
+//
+// let s = areaR(10);
+// console.log(s);
+
+// function areaC(h,r) {
+//
+//     let result = 2 * 3.14 * r * (r + h);
+//     return result;
+// }
+// let number = areaC(10, 8);
+// console.log(number);
+
+// let numbersArray1 = [1, 4, 2, 5, 7, 5, 4, 3, 6, 6, 7, 6, 4, 3,];
+//
+// function array(numb) {
+//     for (const numbers1 of numb) {
+//         console.log(numbers1);
+//
+//     }
+// }
+// array(numbersArray1);
+
+// function text(sim) {
+//
+//     document.write(`<p>${sim}</p>`)
+//
+// }
+// text("dihsdkhvdsvdsf");
+// text("kfvdfvdfkvdfbvd");
+
+// function ualka(text) {
+//
+//     document.write(`<ul> <li>${text}</li>
+//                          <li>${text}</li>
+//                          <li>${text}</li>
+//     </ul>`)
+//
+// }
+// ualka("hallo1");
+
+// function iter(text,numb){
+//     document.write(`<ul>`)
+//     for (let i = 0 ; i < numb; i++){
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`)
+// }
+// iter('hallo',7);
+
+// function arrayLog(array) {
+//
+//     for (let i = 0; i<array.length;i++){
+//        console.log(array[i]);
+//     }
+//
+// }
+//
+// let users = [
+//     {
+//         id: 2343,
+//         name: "Oleksandr",
+//         age: 20,
+//     },
+//     {
+//         id: 6564,
+//         name: "Ivan",
+//         age: 24,
+//     },
+//     {
+//         id: 23121,
+//         name: "Sergiy",
+//         age: 26,
+//     },
+// ]
+//
+// arrayLog(users);
+
+// function getMaxOfArray(numArray) {
+//     return Math.min.apply(null, numArray);
+// }
+// let array = [23,63,132,57,11];
+// let number = getMaxOfArray(array);
+// console.log(number);
+
+// function sum (arr){
+//     let s = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         s += arr[i]
+//     }
+//     return s
+// }
+//
+// let array = [1,5,2,5,7,10];
+// console.log(sum(array));
+
+
