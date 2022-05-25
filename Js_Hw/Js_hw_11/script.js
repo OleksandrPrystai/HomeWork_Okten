e9d57129eee374e049294050821b673a47140e63
OleksandrPrystai <prystaioleksandr@gmail.com>
2022-05-26 01:32:16 +0300

    Js homework 11

---
+++ Js_Hw/Js_hw_11/script.js
@@ -0,0 +1,43 @@
+// -створити форму з інпутами для name та age.
+// При відправці форми записати об'єкт в localstorage
+//
+//
+// let name = document.forms.userForm.name;
+// let age = document.forms.userForm.age;
+// let butter = document.getElementById('butter');
+//
+// butter.onclick = function (e){
+//     e.preventDefault()
+//     let nameKey = name.value
+//     let ageKey = age.value
+//     let obj = {
+//         name:nameKey,
+//         age:ageKey
+//     }
+//     localStorage.setItem('User', JSON.stringify(obj))
+// }
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+//
+// const model = document.forms.formCar.model;
+// const type = document.forms.formCar.type;
+// const volume = document.forms.formCar.volume;
+// const butt = document.getElementById('butter');
+//
+// butt.onclick = function (e) {
+//     e.preventDefault();
+//     let modelKey = model.value
+//     let typeKey = type.value
+//     let volumeKey = volume.value
+//     let arr = [
+//         {
+//             model: modelKey,
+//             type: typeKey,
+//             volume: volumeKey
+//         }
+//     ]
+//     localStorage.setItem('car',JSON.stringify(arr));
+// }
+
+
